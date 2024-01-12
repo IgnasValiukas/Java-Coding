b3bd7975a1243a4f72d690aa27fe0a72af31482b
Ignas <89981814+IgnasValiukas@users.noreply.github.com>
2024-01-12 20:45:27 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public void main() {
-        String str = "Ignas";
-        String reverse = "";
-        int length = str.length();
-        for(int i=0; i<length; i++){
-            reverse = str.charAt(i) + reverse;
-        }
-        System.out.println(reverse);
-}
